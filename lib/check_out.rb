31427b6dfcada04c829d318c6a2d1cd6a78760cb
Vahdet Pervan <vpervan22@gmail.com>
2025-10-30 18:14:56 +0100

    No need for .tally call, @items can just be a hash.

--- lib/check_out.rb
+++ lib/check_out.rb
@@ -1,11 +1,11 @@
 class CheckOut
   def initialize(rules)
     @rules = rules
-    @items = []
+    @items = Hash.new(0)
   end
 
   def total
-    @items.tally.sum do |item, quantity|
+    @items.sum do |item, quantity|
       product = @rules[item].new
       offer_rules = product.special_offer_rules
       quotient, modulus = offer_rules[:quantity].zero? ? [0, quantity] : quantity.divmod(offer_rules[:quantity])
@@ -13,5 +13,5 @@ class CheckOut
     end
   end
 
-  def scan(item) = @items << item
+  def scan(item) = @items[item] += 1
 end
