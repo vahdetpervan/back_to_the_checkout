d903cb32ba4165f34bc91f089195e1da3fcae815
Vahdet Pervan <vpervan22@gmail.com>
2025-10-30 18:11:54 +0100

    Rename product_obj to product

--- lib/check_out.rb
+++ lib/check_out.rb
@@ -6,10 +6,10 @@ class CheckOut
 
   def total
     @items.tally.sum do |item, quantity|
-      product_obj = @rules[item].new
-      offer_rules = product_obj.special_offer_rules
+      product = @rules[item].new
+      offer_rules = product.special_offer_rules
       quotient, modulus = offer_rules[:quantity].zero? ? [0, quantity] : quantity.divmod(offer_rules[:quantity])
-      (offer_rules[:price] * quotient) + (product_obj.unit_price * modulus)
+      (offer_rules[:price] * quotient) + (product.unit_price * modulus)
     end
   end
 
